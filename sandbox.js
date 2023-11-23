a9d3646432e1e02ad410b20706e9b31e9e902eb2
Htut Aung Wai <defo19987@gmail.com>
2023-11-23 09:31:26 +0700

    finished tutorial 5

--- sandbox.js
+++ sandbox.js
@@ -5,10 +5,47 @@ age = 34;
 var ninjas;
 ninjas = ["luigi", "mario"];
 ninjas.push("3");
-var mixed = [];
-mixed.push("hello");
-mixed.push(22);
+var mixedArray = [];
+mixedArray.push("hello");
+mixedArray.push(22);
 var circ = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circ(3));
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+var numbers = [10, 20, 30, 40, 50];
+numbers.push(60);
+// numbers.push("60") ERROR
+var mixed = ["ken", 5, "toad", 78];
+mixed.push("princess");
+mixed.push(50);
+// mixed.push(true); ERROR
+// OBJECTS
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 40,
+};
+ninja.age = 40;
+ninja.name = "htut";
+// ninja.belt = 44; ERROR
+// let students: string[]; ---variable `students` gets defined, but it has no assigned value - it is defined but unassigned
+var students = [];
+students.push("shaun");
+// UNIONS
+var mixedUnion = [];
+mixedUnion.push("hello");
+mixedUnion.push(300);
+mixedUnion.push(false);
+var uid;
+uid = "2fjojfoafjS";
+uid = 2343324;
+var ninjaOne;
+ninjaOne = { name: "yoshi", age: 23, married: false };
+var ninjaTwo;
+ninjaTwo = {
+    name: "aung",
+    age: 33,
+    married: true,
+};
--- sandbox.ts
+++ sandbox.ts
@@ -9,13 +9,71 @@ let ninjas: string[];
 ninjas = ["luigi", "mario"];
 ninjas.push("3");
 
-let mixed: (string | number)[] = [];
+let mixedArray: (string | number)[] = [];
 
-mixed.push("hello");
-mixed.push(22);
+mixedArray.push("hello");
+mixedArray.push(22);
 
 const circ = (diameter: number) => {
   return diameter * Math.PI;
 };
 
 console.log(circ(3));
+
+let names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+
+let numbers = [10, 20, 30, 40, 50];
+numbers.push(60);
+
+// numbers.push("60") ERROR
+
+let mixed = ["ken", 5, "toad", 78];
+mixed.push("princess");
+mixed.push(50);
+
+// mixed.push(true); ERROR
+
+// OBJECTS
+let ninja = {
+  name: "mario",
+  belt: "black",
+  age: 40,
+};
+
+ninja.age = 40;
+ninja.name = "htut";
+// ninja.belt = 44; ERROR
+
+// let students: string[]; ---variable `students` gets defined, but it has no assigned value - it is defined but unassigned
+
+let students: string[] = [];
+students.push("shaun");
+
+// UNIONS
+
+let mixedUnion: (string | number | boolean)[] = [];
+
+mixedUnion.push("hello");
+mixedUnion.push(300);
+mixedUnion.push(false);
+
+let uid: string | number;
+
+uid = "2fjojfoafjS";
+uid = 2343324;
+
+let ninjaOne: object;
+ninjaOne = { name: "yoshi", age: 23, married: false };
+
+let ninjaTwo: {
+  name: string;
+  age: number;
+  married: boolean;
+};
+
+ninjaTwo = {
+  name: "aung",
+  age: 33,
+  married: true,
+};
