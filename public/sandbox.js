063bddebe5092fe5052e8042b73524e55278259c
Htut Aung Wai <defo19987@gmail.com>
2023-11-24 13:17:38 +0700

    finished tutorial 11

--- public/sandbox.js
+++ public/sandbox.js
@@ -3,6 +3,23 @@
 const logDetails = (uid, item) => {
     console.log(`${item} with id: ${uid}`);
 };
+let greet;
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === "add") {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+greet = (name, greeting) => {
+    console.log(`${name} says ${greeting}`);
+};
 const greetAgain = (user) => {
     console.log(`${user.name} is ${user.age} years old`);
+};
+let logIn;
+logIn = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old.`);
 };
